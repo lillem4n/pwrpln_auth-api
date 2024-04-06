d051657b77696dab79d9cc77bbbfa2c476229ddc
lilleman <lilleman@larvit.se>
2024-04-06 14:35:44 +0200

    Indent fixes

--- .gitea/test-and-build.sh
+++ .gitea/test-and-build.sh
@@ -9,15 +9,15 @@ curl \
 {
 	"commands": [
 		"git clone --single-branch --branch ${GITHUB_REF_NAME} --depth 1 ssh://git@gitea.larvit.se:21022/pwrpln/auth-api.git",
-        "cd auth-api",
-        "docker compose build",
-        "docker compose --profile tests build",
-        "docker compose run --rm tests",
-        "docker compose down -v --remove-orphans -t0",
+		"cd auth-api",
+		"docker compose build",
+		"docker compose --profile tests build",
+		"docker compose run --rm tests",
+		"docker compose down -v --remove-orphans -t0",
 		"echo \"${DOCKER_PASSWORD}\" | docker login gitea.larvit.se --username ${DOCKER_USERNAME} --password-stdin",
 		"docker build -t gitea.larvit.se/pwrpln/auth-api:${GITHUB_REF_NAME} .",
 		"docker push gitea.larvit.se/pwrpln/auth-api:${GITHUB_REF_NAME}",
-        "docker build -t gitea.larvit.se/pwrpln/auth-api-db-migrations:${GITHUB_REF_NAME} -f ./Dockerfile.migrations .",
+		"docker build -t gitea.larvit.se/pwrpln/auth-api-db-migrations:${GITHUB_REF_NAME} -f ./Dockerfile.migrations .",
 		"docker push gitea.larvit.se/pwrpln/auth-api-db-migrations:${GITHUB_REF_NAME}"
 	]
 }
